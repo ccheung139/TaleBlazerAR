a83462924337d46789994fb12d5714be27f07224
Christopher <ccheung9@mit.edu>
2020-09-06 22:14:35 -0400

    Adding object placement fun to clock

---
+++ ClockAR/Assets/Scripts/ObjectPlacementScript.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARRaycastManager))]
+public class ObjectPlacementScript : MonoBehaviour
+{
+    private ARRaycastManager raycastManager;
+    private GameObject spawnedObject;
+
+    [SerializeField]
+    public GameObject PlaceablePrefab;
+
+    static List<ARRaycastHit> s_Hits = new List<ARRaycastHit>();
+
+    private void Awake()
+    {
+        raycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    bool TryGetTouchPosition(out Vector2 touchPosition)
+    {
+        if(Input.touchCount > 0)
+        {
+            touchPosition = Input.GetTouch(0).position;
+            return true;
+        }
+        touchPosition = default;
+        return false;
+    }
+
+    private void Update()
+    {
+        if(!TryGetTouchPosition(out Vector2 touchPosition))
+        {
+            return;
+        }
+        if(raycastManager.Raycast(touchPosition, s_Hits, TrackableType.PlaneWithinPolygon))
+        {
+            var hitPose = s_Hits[0].pose;
+            if(spawnedObject == null) {
+                spawnedObject = Instantiate(PlaceablePrefab, hitPose.position, Quaternion.Euler(0, 0, 0));
+            }
+            else
+            {
+                spawnedObject.transform.position = hitPose.position;
+            }
+        }
+    }
+
+}
+
